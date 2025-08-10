13b9bf8aac04eaaa6a066b224f7e208cd7332e0f
Kevin Benavides <126353237+DevCheckOG@users.noreply.github.com>
2025-08-10 11:52:55 -0600

    Update malloc.h

--- malloc.h
+++ malloc.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 
+#define MIN_BLOCK_SIZE 16
+#define ALIGNMENT 8
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
+#define HEADER_SIZE (sizeof(Block))
+
 void *malloc(size_t size);
 void free(void *ptr);
 
